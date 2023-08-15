59589f7294b1e5e64ddb4193a85dcebcc5026d4c
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-08-15 18:08:09 +0000

    p1

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="realtiveList">
+                  <li key="relativeListItem1">Shiva</li>
+                  <li key="relativeListItem2">Vishnu</li>
+                  <li key="relativeListItem3">Parvathi</li>
+                  <li key="relativeListItem4">Maheshwara</li>
+               </ol>
     </div>
   )
 }
